5262949b17aac84efa3386628b9c67f31a788fd1
dmitriy levykin <dmlegasy@gmail.com>
2026-01-10 21:20:14 +0300

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+from src.auth.dependencies import UserInDBDep
+from src.auth.middleware import access_middleware
+from src.auth.router import router as auth_router
+
+app = FastAPI()
+app.middleware("http")(access_middleware)
+app.include_router(auth_router)
+
+
+@app.get("/users/me")
+async def profile(user: UserInDBDep):
+    return user
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
