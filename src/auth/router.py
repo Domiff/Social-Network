5cd3c97f9fef314932e327a13f86124966d22957
dmitriy levykin <dmlegasy@gmail.com>
2026-01-22 20:33:10 +0300

    Add jinja2 templates for registration, login and logout

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,14 +1,20 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Request
 
 from src.auth.config import settings
 from src.auth.dependencies import DataFormDep, OAuth2PasswordRequestFormDep
 from src.auth.services import authenticate_user, create_tokens, create_user
 from src.database import SessionDep
+from src.templates import templates
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@router.post("/register")
+@router.get("/registration-page")
+async def registration_page(request: Request):
+    return templates.TemplateResponse("auth/registration.html", {"request": request})
+
+
+@router.post("/registration")
 async def registration(data: DataFormDep, session: SessionDep, response: Response):
     user = await create_user(data, session)
     tokens = create_tokens(user)
@@ -29,6 +35,11 @@ async def registration(data: DataFormDep, session: SessionDep, response: Respons
         samesite="strict",
     )
     return {"detail": "Registered in successfully"}
+
+
+@router.get("/login-page")
+async def login_page(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
 
 
 @router.post("/login")
@@ -53,6 +64,11 @@ async def login(
         samesite="strict",
     )
     return {"detail": "Logged in successfully"}
+
+
+@router.get("/logout-page")
+async def logout_page(request: Request):
+    return templates.TemplateResponse("auth/logout.html", {"request": request})
 
 
 @router.post("/logout")
