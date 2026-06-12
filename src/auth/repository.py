d439a9316434a76d6bae5527616afb60b6b8a60a
dmitriy levykin <dmlegasy@gmail.com>
2026-06-12 16:16:40 +0300

    refactor(auth): add try/except for list and get_by_email

--- src/auth/repository.py
+++ src/auth/repository.py
@@ -23,14 +23,22 @@ class UserRepository(BaseRepository):
             raise AlreadyExists() from e
 
     async def list(self) -> list[User]:
-        query = select(User)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+        try:
+            query = select(User)
+            result = await self.session.execute(query)
+            return result.scalars().all()
+        except IntegrityError as e:
+            logger.error("User does not exist")
+            raise DoesNotExists() from e
 
-    async def get_by_email(self, email: str) -> User | None:
-        query = select(User).where(User.email == email, User.is_active)
-        result = await self.session.execute(query)
-        return result.scalar_one_or_none()
+    async def get_by_email(self, email: str) -> User:
+        try:
+            query = select(User).where(User.email == email, User.is_active)
+            result = await self.session.execute(query)
+            return result.scalar_one()
+        except IntegrityError as e:
+            logger.error("User does not exist")
+            raise DoesNotExists() from e
 
     async def delete(self, email: str, is_soft: bool) -> bool:
         try:
