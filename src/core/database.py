bf8460a1297eb2f75f2001ee75b5df62b9a02356
dmitriy levykin <dmlegasy@gmail.com>
2026-09-05 20:45:09 +0300

    feat(database): add NAMING_CONVENTION

--- src/core/database.py
+++ src/core/database.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy import func, text
+from sqlalchemy import func, text, MetaData
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -16,9 +16,19 @@ from src.core.logging import get_logger
 
 logger = get_logger(__name__)
 
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=NAMING_CONVENTION)
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
